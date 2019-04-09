11ff63e99d1ae4ace84fe72ad2961bbde26d4a35
Denis Slavik <denis.slavik@gmail.com>
2019-04-09 13:09:03 +0200

    Fixed export declaration;

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,19 @@
 import { Component } from 'react';
 
 export interface BarcodeMaskProps {
-  width?: number | string;
-  height?: number | string;
-  edgeWidth?: number | string;
-  edgeHeight?: number | string;
-  edgeColor?: string;
-  edgeBorderWidth?: number | string;
-  transparency?: number;
-  showAnimatedLine?: boolean;
-  animatedLineColor?: string;
-  animatedLineHeight?: number;
-  lineAnimationDuration?: number;
+	width?: number | string;
+	height?: number | string;
+	edgeWidth?: number | string;
+	edgeHeight?: number | string;
+	edgeColor?: string;
+	edgeBorderWidth?: number | string;
+	transparency?: number;
+	showAnimatedLine?: boolean;
+	animatedLineColor?: string;
+	animatedLineHeight?: number;
+	lineAnimationDuration?: number;
 }
 
-export class BarcodeMask extends Component<BarcodeMaskProps, {}> {}
+declare class BarcodeMask extends Component<BarcodeMaskProps, {}> {}
 
 export default BarcodeMask;
